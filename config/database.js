47342e024f3ea51c25100b163f0acee9b3f2729d
ozan.koyuk <ozan.koyuk@spp42.com>
2020-08-11 17:14:05 +0300

    mongo connection changed to live

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    localUrl: 'mongodb://localhost/testarea'
-    //localUrl: "mongodb+srv://challengeUser:WUMglwNBaydH8Yvu@challenge-xzwqd.mongodb.net/getir-case-study?retryWrites=true"
+    //localUrl: 'mongodb://localhost/testarea'
+    localUrl: "mongodb+srv://challengeUser:WUMglwNBaydH8Yvu@challenge-xzwqd.mongodb.net/getir-case-study?retryWrites=true"
 
 };
